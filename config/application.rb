41769b7678e237bc4c6bac3e7c7472b16a63e9d5
Andrew Mason <andrewmcodes@protonmail.com>
2019-08-23 00:00:34 -0400

    remove unused files
    delete scaffold files

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,12 @@ module StimulusTodomvc
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.template_engine nil # to skip views
+      g.test_framework  nil # to skip test framework
+      g.assets false
+      g.helper false
+      g.stylesheets false
+    end
   end
 end
