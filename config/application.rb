8d328a50d424b0f7339e6b1bbd520eb8ef59ddd0
Andrew Mason <andrewmcodes@protonmail.com>
2019-09-03 16:25:58 -0400

    add linters (#4)
    add standardrb and prettier. Lint all the things.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,7 +15,7 @@ module StimulusTodomvc
     # helpers, and stylesheets will not be created
     config.generators do |g|
       g.template_engine nil
-      g.test_framework  nil
+      g.test_framework nil
       g.assets false
       g.helper false
       g.stylesheets false
