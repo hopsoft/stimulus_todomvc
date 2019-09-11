33a894302dcf78bc66f85324d0a9f7bf6e5a1e08
Andrew Mason <andrewmcodes@protonmail.com>
2019-09-11 19:14:21 -0400

    remove unneeded target

--- app/javascript/controllers/todos_controller.js
+++ app/javascript/controllers/todos_controller.js
@@ -2,7 +2,7 @@ import Rails from '@rails/ujs';
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = ['bulkUpdateForm', 'form'];
+  static targets = ['form'];
 
   toggleAll(event) {
     Rails.handleRemote.call(this.formTarget, event);
