5b94349d7bcb1c071701d321262f69fcf9fc71e5
Tommy Chester <thomas.charles.chester@gmail.com>
2021-02-24 12:55:27 -0600

    Now I have a CRUD and all show and remove all completed is finished

--- app/controllers/api/v1/todos.rb
+++ app/controllers/api/v1/todos.rb
@@ -3,12 +3,48 @@ module API
         class Todos < Grape::API
             include API::V1::Defaults
 
-            resource :todos do
-                desc "All Available Todos"
-                get "" do 
-                    Todo.all
-                end
+            
 
+            resource :todos do
+
+                #Made route to get all 
+                desc "All Available Todos"
+                    get "" do 
+                        Todo.all
+                    end
+
+                #Route to destory all completed (would never actually do this and instead keep the data, and make "show or not show") 
+                desc "All Available Todos"
+                    delete "remove_complete" do 
+                        Todo.where("completed").destroy_all
+                    end
+
+                #Post Route to create todo   
+                desc "create a Todo"
+                    params do
+                        requires :title, type: String, desc: 'Create a todo'
+                    end
+                    post do 
+                        Todo.create!({
+                            title: params[:title],
+                            session_id: params[:session_id]
+                        })
+                    end
+                
+                #Update route to flip completion
+                desc "Flip Todo Creation"
+                    params do
+                        requires :id
+                    end
+                    put ':id' do
+                        current_todo = Todo.find_by_id(params[:id])
+                        #If not found the api will respond with:
+                        return "Todo Not Found" unless current_todo
+                        current_todo.completed = !current_todo.completed
+                        current_todo.save!
+                    end
+
+                # return Single Todo
                 desc "Return a Todo"
                     params do 
                         requires :id, type: String, desc: "ID the TODO"
@@ -16,7 +52,17 @@ module API
                     get ":id" do
                         Todo.where(id: permitted_params[:id]).first!
                     end
-                end
+
+                # Destroy single todo ( I would not actually destroy in real world, just hide or give an "are you sure")
+                desc "Remove Todo"
+                    params do 
+                        requires :id
+                    end
+                    delete ":id" do 
+                        Todo.find_by_id(params[:id]).destroy!
+                    end
+                    
+            end
         end
     end    
 end
